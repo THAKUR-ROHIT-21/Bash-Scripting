1d29b1ca60b6b6996c98a6691844de1f757874a8
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-27 16:52:53 +0000

    in Hindi

---
+++ script_for_loops/normalfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "पहला नंबर डालें :- " num1
+read -p "दूसरा नंबर डालें :- " num2
+
+
+for i in $(seq $num1 $num2);
+do
+	if (( i % 2 == 0 ))
+        then
+           echo "$i एक सम नंबर है"
+        else
+           echo "$i एक विषम नंबर है"
+        fi
+done
