bca9ab23aa472dba404833e9239ec50d37ac2541
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-27 16:59:13 +0000

    Multiple Function Work

--- script_for_loops/normalfile.sh
+++ script_for_loops/normalfile.sh
@@ -4,12 +4,12 @@ read -p "पहला नंबर डालें :- " num1
 read -p "दूसरा नंबर डालें :- " num2
 
 
-for i in $(seq $num1 $num2);
+for i  in $(seq $num1 $num2);
 do
-	if (( i % 2 == 0 ))
+	if (( i  % 2 == 0 ))
         then
-           echo "$i एक सम नंबर है"
+           echo "$i  एक सम नंबर है"
         else
-           echo "$i एक विषम नंबर है"
+           echo "$i  एक विषम नंबर है"
         fi
 done
